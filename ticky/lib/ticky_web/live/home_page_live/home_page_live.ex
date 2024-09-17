8bba4d46848749643d5c23f0ebc9084c1e3b8918
Chris <fitzer.c@gmail.com>
2024-09-16 22:30:07 -0500

    changing filter to alternate between all and today

--- ticky/lib/ticky_web/live/home_page_live/home_page_live.ex
+++ ticky/lib/ticky_web/live/home_page_live/home_page_live.ex
@@ -4,11 +4,16 @@ defmodule TickyWeb.HomePageLive do
   alias Ticky.{ Timers, TimeEntries }
 
   def mount(_params, _session, socket) do
+    timezone = get_connect_params(socket)["timezone"] || "UTC"
+    socket = assign(socket, timezone: timezone)
+
     user_timers = Timers.list_users_timers(socket.assigns.current_user.id)
     socket = assign(socket, timers: user_timers)
 
-    user_time_entries = TimeEntries.list_users_time_entries(socket.assigns.current_user.id)
+    user_time_entries = get_todays_time_entries(socket)
     socket = assign(socket, time_entries: user_time_entries)
+
+    socket = assign(socket, filter: "today")
 
     {:ok, socket}
   end
@@ -22,10 +27,28 @@ defmodule TickyWeb.HomePageLive do
   end
 
   def handle_event("show_today", _value, socket) do
-    date = Date.utc_today()
-    datetime = DateTime.new!(date, ~T[00:00:00])
-    todays_entries = TimeEntries.get_time_entries_for_day(socket.assigns.current_user.id, datetime)
+    todays_entries = get_todays_time_entries(socket)
     socket = assign(socket, time_entries: todays_entries)
+
+    socket = assign(socket, filter: "today")
+
+    {:noreply, socket}
+  end
+
+  defp get_todays_time_entries(socket) do
+    timezone = socket.assigns.timezone
+    date = Timex.now(timezone)
+    datetime = Timex.set(date, hour: 0, minute: 0, second: 0)
+
+    TimeEntries.get_time_entries_for_day(socket.assigns.current_user.id, datetime)
+  end
+
+  def handle_event("show_all", _value, socket) do
+    user_time_entries = TimeEntries.list_users_time_entries(socket.assigns.current_user.id)
+    socket = assign(socket, time_entries: user_time_entries)
+
+    socket = assign(socket, filter: "all")
+
     {:noreply, socket}
   end
 end
